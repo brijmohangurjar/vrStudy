84e0247eab9a6910ce21cbf03f3edf34d49482e1
brij <braj.m@brainAbove.io>
2022-06-11 15:19:22 +0530

    update

--- mobile/src/app/modules/home/home.page.ts
+++ mobile/src/app/modules/home/home.page.ts
@@ -11,6 +11,9 @@ import { NavigationService, ToastService } from 'src/app/service';
 export class HomePage implements OnInit {
 
   public subjectList = [];
+  public pageList = [];
+  public shortList = [];
+
   public currentDate = new Date();
 
   opts = {
@@ -112,6 +115,8 @@ export class HomePage implements OnInit {
 
   public ngOnInit() {
     this.getSubjectsList();
+    this.getPageList();
+    this.getShortList();
   }
 
 
@@ -130,8 +135,31 @@ export class HomePage implements OnInit {
       });
   }
 
+  
+  private getPageList(): void {
+    this.homeService.getPageList()
+      .subscribe((result: any) => {
+        if (result && result.length) {
+          this.pageList = result;
+        }
+      }, (error: HttpErrorResponse) => {
+        this.toastService.errorToast(error.message);
+      });
+  }
+
+    
+  private getShortList(): void {
+    this.homeService.getShortList()
+      .subscribe((result: any) => {
+        if (result && result.length) {
+          this.shortList = result;
+        }
+      }, (error: HttpErrorResponse) => {
+        this.toastService.errorToast(error.message);
+      });
+  }
+
   public clickOnSubject(item: any): void {
-    console.log('item', item);
     this.navigationService.navigateByRelativePath('subject', item.docId)
   }
 }
