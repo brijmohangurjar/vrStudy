8b0286a0e90b5df0e550bd72af58dbd465506869
Nilesh Ratre <ratrenilesh02@gmail.com>
2022-06-15 22:26:49 +0530

    update

--- mobile/src/app/guard/is-logged-in-guard.ts
+++ mobile/src/app/guard/is-logged-in-guard.ts
@@ -8,7 +8,6 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ConstantVariables } from 'src/const/constant';
-import { LoginService } from '../api-services';
 
 @Injectable()
 export class IsLoggedInGuard implements CanActivate {
@@ -16,7 +15,6 @@ export class IsLoggedInGuard implements CanActivate {
   constructor(
     private router: Router,
     private afAuth: AngularFireAuth,
-    private loginService: LoginService,
     public constVar: ConstantVariables
   ) { }
 
@@ -27,19 +25,8 @@ export class IsLoggedInGuard implements CanActivate {
     return new Promise((resolve, reject) => {
       this.afAuth.authState.subscribe((user: any) => {
         if (user) {
-          this.loginService.getUserByAuthIdWithPromise(user.uid)
-            .then((userData: any) => {
-              console.log('userData', userData);
-              if (userData && userData.length) {
-                this.router.navigate(['base']);
-                resolve(false);
-              } else {
-                localStorage.clear();
-                this.afAuth.signOut();
-                this.router.navigate(['login']);
-                resolve(false);
-              }
-            });
+          this.router.navigate(['base']);
+          resolve(false);
         } else {
           resolve(true);
         }
